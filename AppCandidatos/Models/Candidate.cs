87d80d8881fd533ddda25dd2aaabb7a421260c0e
leonardocode <36961974+leonardocode@users.noreply.github.com>
2024-09-17 17:06:34 -0500

    se realizan ajustes en los displays de las entidades

--- AppCandidatos/Models/Candidate.cs
+++ AppCandidatos/Models/Candidate.cs
@@ -5,21 +5,26 @@ namespace AppCandidatos.Models
     public class Candidate
     {
         [Key]
+        [Display(Name = "Id Candidato")]
         public int IdCandidate { get; set; }
 
+        [Display(Name = "Nombre Candidato")]
         [Required(ErrorMessage = "El nombre es obligatorio")]
         [MaxLength(50, ErrorMessage = "El nombre no puede tener más de 50 caracteres")]
         public string Name { get; set; }
 
+        [Display(Name = "Apellido Candidato")]
         [Required(ErrorMessage = "El apellido es obligatorio")]
         [MaxLength(150, ErrorMessage = "El apellido no puede tener más de 150 caracteres")]
         public string Surname { get; set; }
+
 
         [Required(ErrorMessage = "La fecha de nacimiento es obligatoria")]
         [DataType(DataType.Date, ErrorMessage = "El formato de la fecha de nacimiento no es válido")]
         [Display(Name = "Fecha de Nacimiento")]
         public DateTime Birthdate { get; set; }
 
+        [Display(Name = "Email Candidato")]
         [Required(ErrorMessage = "El correo electrónico es obligatorio")]
         [EmailAddress(ErrorMessage = "Formato de correo electrónico inválido")]
         [MaxLength(250, ErrorMessage = "El correo no puede tener más de 250 caracteres")]
--- AppCandidatos/Models/CandidateExperience.cs
+++ AppCandidatos/Models/CandidateExperience.cs
@@ -5,27 +5,35 @@ namespace AppCandidatos.Models
     public class CandidateExperience
     {
         [Key]
+        [Display(Name = "Id Experiencia Candidato")]
         public int IdCandidateExperience { get; set; }
 
+        [Display(Name = "Empresa Candidato")]
         [Required(ErrorMessage = "El nombre de la empresa es obligatorio")]
         [MaxLength(100, ErrorMessage = "El nombre de la empresa no puede tener más de 100 caracteres")]
         public string Company { get; set; }
 
+        [Display(Name = "Empleo Candidato")]
         [Required(ErrorMessage = "El cargo es obligatorio")]
         [MaxLength(100, ErrorMessage = "El cargo no puede tener más de 100 caracteres")]
         public string Job { get; set; }
 
+
+        [Display(Name = "Descripcion Candidato")]
         [MaxLength(4000, ErrorMessage = "La descripción no puede tener más de 4000 caracteres")]
         public string Description { get; set; }
 
-        [Range(0, 999999, ErrorMessage = "El salario debe estar entre 0 y 999,999")]
+
+        [Display(Name = "Sueldo Candidato")]
         [DataType(DataType.Currency, ErrorMessage = "El formato de salario no es válido")]
         public decimal Salary { get; set; }
+
 
         [Required(ErrorMessage = "La fecha de inicio es obligatoria")]
         [DataType(DataType.Date, ErrorMessage = "El formato de la fecha de inicio no es válido")]
         [Display(Name = "Fecha de Inicio")]
         public DateTime BeginDate { get; set; }
+
 
         [DataType(DataType.Date, ErrorMessage = "El formato de la fecha de fin no es válido")]
         [Display(Name = "Fecha de Fin")]
